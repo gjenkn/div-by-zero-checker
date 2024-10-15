d1d5c7691a7159e184a96b36f2a60983c79243f8
Rebecca <rebeccamhu4@gmail.com>
2024-10-15 10:49:32 -0400

    removed top.java, changed unknown.java to be the bottom

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/PossiblyZero.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/PossiblyZero.java
@@ -3,7 +3,14 @@ package org.checkerframework.checker.dividebyzero.qual;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
+import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 
-@SubtypeOf(Top.class)
+/**
+ * You do not want a qualifier `@Top`. It appears here as a placeholder, so that the project
+ * compiles.
+ */
+
+@DefaultQualifierInHierarchy
+@SubtypeOf()
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface PossiblyZero { }
